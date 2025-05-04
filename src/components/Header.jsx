eb5dd23992732ace95b55a0569b8e01748bb2c15
MannexDera <emmanuelchi545@gmail.com>
2025-05-04 23:58:12 +0100

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,16 @@ import { useState } from 'react';
 
 export default function Header() {
     return (
-        <div className="flex justify-between items-center">
-            <h1 className="uppercase text-light-veryLightGray text-4xl font-bold dark:text-dark-veryDarkBlue tracking-[1.5rem]">
+        <div className="flex justify-between">
+            <h1 className="uppercase text-light-veryLightGray text-3xl font-bold tracking-[.8rem]">
                 Todo
             </h1>
 
             <img
                 src="/images/icon-moon.svg"
                 alt="Moon Icon"
-                className="w-8 h-8"
+                className="w-6 h-6"
             />
         </div>
     );
 }
-
