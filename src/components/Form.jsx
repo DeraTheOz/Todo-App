c05bb50842c0b439e5226b3b491167e739d40000
MannexDera <emmanuelchi545@gmail.com>
2025-05-04 23:57:41 +0100

    add todo form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,54 @@
 export default function Form() {
     return (
-        <form className="">
-            <div className="flex items-center gap-4 w-80 mt-8 mb-4 bg-light-veryLightGray p-4 rounded-md">
-                <input type="checkbox" className="w-4 h-4 rounded-full" />
+        <form
+            className="flex items-center gap-4 mt-8 mb-4 px-4 py-3 bg-light-veryLightGray 
+                dark:bg-dark-veryDarkDesaturatedBlue rounded-md">
+            <label
+                htmlFor="check-box"
+                className="flex items-center cursor-pointer relative">
                 <input
-                    type="text"
-                    placeholder="Create a new todo"
-                    className="inline-block"
+                    type="checkbox"
+                    id="check-box"
+                    className="w-6 h-6 appearance-none cursor-pointer transition 
+                        border border-light-veryLightgrayishBlue dark:border-dark-veryDarkGrayishBlue1 rounded-full"
                 />
-            </div>
+                <img
+                    src="/images/icon-check.svg"
+                    alt="Check"
+                    className="check w-3 h-3 absolute left-1.5 opacity-0 transition"
+                />
+            </label>
+
+            <input
+                type="text"
+                placeholder="Create a new todo"
+                className="w-full px-2 text-light-veryDarkGryishBlue dark:text-dark-lightGrayishBlue dark:placeholder-dark-darkGrayishBlue bg-light-veryLightGray 
+                    dark:bg-inherit border-none outline-0"
+            />
         </form>
     );
 }
+
+// <div className="flex items-center gap-4 mt-8 mb-4 px-6 py-6 bg-light-veryLightGray dark:bg-dark-desaturatedBlue rounded-md">
+//                 <label className="flex items-center cursor-pointer">
+//                     <div
+//                         className="relative w-7 h-7 rounded-full border border-light-veryLightgrayishBlue dark:border-dark-veryDarkGrayishBlue1
+//                         bg-transparent peer-checked:bg-checkbox-bg flex items-center justify-center transition-colors duration-300 overflow-hidden">
+//                         <input
+//                             type="checkbox"
+//                             className="w-full h-full absolute opacity-0 cursor-pointer peer"
+//                         />
+//                         {/*<img
+//                             src="/images/icon-check.svg"
+//                             alt="Check"
+//                             className="w-3 h-3 opacity-0 peer-checked:opacity-100 transition-opacity duration-300 pointer-events-none"
+//                         />*/}
+//                     </div>
+//                 </label>
+
+//                 <input
+//                     type="text"
+//                     placeholder="Create a new todo"
+//                     className="w-full px-2 text-light-veryDarkGrayishBlue dark:text-dark-lightGrayishBlue dark:placeholder-dark-darkGrayishBlue bg-light-veryLightGray dark:bg-inherit border-none outline-0"
+//                 />
+//             </div>
