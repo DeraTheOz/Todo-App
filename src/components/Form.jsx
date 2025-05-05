ae0921b594f0a50494ca14391ad3f11a159a9778
MannexDera <emmanuelchi545@gmail.com>
2025-05-05 18:05:15 +0100

    fix: fix bad code

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,7 +8,7 @@ export default function Form() {
                 className="flex items-center cursor-pointer relative">
                 <input
                     type="checkbox"
-                    id="check-box"
+                    id="checkbox"
                     className="w-6 h-6 appearance-none cursor-pointer transition 
                         border border-light-veryLightgrayishBlue dark:border-dark-veryDarkGrayishBlue1 rounded-full"
                 />
@@ -29,26 +29,3 @@ export default function Form() {
     );
 }
 
-// <div className="flex items-center gap-4 mt-8 mb-4 px-6 py-6 bg-light-veryLightGray dark:bg-dark-desaturatedBlue rounded-md">
-//                 <label className="flex items-center cursor-pointer">
-//                     <div
-//                         className="relative w-7 h-7 rounded-full border border-light-veryLightgrayishBlue dark:border-dark-veryDarkGrayishBlue1
-//                         bg-transparent peer-checked:bg-checkbox-bg flex items-center justify-center transition-colors duration-300 overflow-hidden">
-//                         <input
-//                             type="checkbox"
-//                             className="w-full h-full absolute opacity-0 cursor-pointer peer"
-//                         />
-//                         {/*<img
-//                             src="/images/icon-check.svg"
-//                             alt="Check"
-//                             className="w-3 h-3 opacity-0 peer-checked:opacity-100 transition-opacity duration-300 pointer-events-none"
-//                         />*/}
-//                     </div>
-//                 </label>
-
-//                 <input
-//                     type="text"
-//                     placeholder="Create a new todo"
-//                     className="w-full px-2 text-light-veryDarkGrayishBlue dark:text-dark-lightGrayishBlue dark:placeholder-dark-darkGrayishBlue bg-light-veryLightGray dark:bg-inherit border-none outline-0"
-//                 />
-//             </div>
