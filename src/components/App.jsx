0429a5df6389cd3cb7c7ec25f5b6df3a1b2495df
MannexDera <emmanuelchi545@gmail.com>
2025-05-05 17:59:49 +0100

    add all components to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,18 @@ import { useState } from 'react';
 import Header from './Header.jsx';
 import Form from './Form.jsx';
 import TodoList from './TodoList.jsx';
-import SortTodo from './SortTodo.jsx';
+import TodoControls from './TodoControls.jsx';
 
 function App() {
     return (
-        <div className="flex flex-col justify-between max-w-[37.5rem] mx-auto translate-y-[5%] lg:translate-y-[20%]">
+        <div className="flex flex-col justify-between max-w-[37.5rem] mx-auto translate-y-[5%] lg:translate-y-[10%]">
             <Header />
             <Form />
             <TodoList />
-            <SortTodo />
+            <TodoControls />
         </div>
     );
+
 }
 
 export default App;
