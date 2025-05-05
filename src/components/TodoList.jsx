8b7209a917de42941af7f7274851b07459c2d2ae
MannexDera <emmanuelchi545@gmail.com>
2025-05-05 18:06:53 +0100

    fix: fix todo container styles

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,7 +2,9 @@ import TodoItem from './TodoItem.jsx';
 
 export default function TodoList() {
     return (
-        <div className="flex flex-col shadow-xl rounded-md mb-6 overflow-hidden">
+        <div
+            className="flex flex-col shadow-xl rounded-t-md overflow-hidden
+            bg-light-veryLightGray dark:bg-dark-veryDarkDesaturatedBlue">
             <div className="max-h-[23.158rem] overflow-scroll">
                 <TodoItem />
                 <TodoItem />
@@ -10,15 +12,6 @@ export default function TodoList() {
                 <TodoItem />
                 <TodoItem />
                 <TodoItem />
-            </div>
-
-            <div
-                className="flex justify-between items-center text-sm text-light-lightGrayishBlue font-semibold px-4 py-6 bg-light-veryLightGray 
-                dark:bg-dark-veryDarkDesaturatedBlue">
-                <p>X items left</p>
-                <p className="hover:text-light-darkGrayishBlue cursor-pointer transition">
-                    Clear Completed
-                </p>
             </div>
         </div>
     );
