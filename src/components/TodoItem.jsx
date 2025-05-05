558308b87e5b49f288bcdc73e01bcf30a9d6c5c1
MannexDera <emmanuelchi545@gmail.com>
2025-05-05 18:06:00 +0100

    fix: fix todo item markup

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,30 +1,31 @@
 export default function TodoItem() {
     return (
-        <div
-            className="flex items-center gap-4 p-4 bg-light-veryLightGray 
-                dark:bg-dark-veryDarkDesaturatedBlue border-b-2 border-light-veryLightGrayishBlue">
+        <div className="todo-item">
             <label
                 htmlFor="check-box"
-                className="flex items-center cursor-pointer relative">
+                className="flex items-center rounded-full cursor-pointer relative">
                 <input
                     type="checkbox"
                     id="check-box"
-                    className="w-6 h-6 appearance-none cursor-pointer transition 
-                        border border-light-veryLightgrayishBlue dark:border-dark-veryDarkGrayishBlue1 rounded-full"
+                    className="check-todo w-6 h-6 appearance-none cursor-pointer transition border border-light-veryLightgrayishBlue
+                     dark:border-dark-veryDarkGrayishBlue1 rounded-full"
                 />
                 <img
                     src="/images/icon-check.svg"
                     alt="Check"
-                    className="check w-3 h-3 absolute left-1.5 opacity-0 transition"
+                    className="check absolute w-3 h-3 translate-x-1/2 opacity-0 transition"
                 />
             </label>
-
-            <p className="w-full px-2 dark:text-dark-lightGrayishBlue">
-                Jog around the park
-            </p>
+            <p className="todo checke">Complete React Course</p>
             <div>
-                <img src="/images/icon-cross.svg" alt="Remove" />
+                <img
+                    src="/images/icon-cross.svg"
+                    alt="Remove"
+                    className="cursor-pointer"
+                />
             </div>
         </div>
     );
+
+
 }
