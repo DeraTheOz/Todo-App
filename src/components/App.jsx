089f081cfdb9b265258d263a9fea0851b0ad3638
MannexDera <emmanuelchi545@gmail.com>
2025-05-06 16:19:12 +0100

    fix: fix import paths

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,13 @@ import TodoControls from './TodoControls.jsx';
 
 function App() {
     return (
-        <div className="flex flex-col justify-between max-w-[37.5rem] mx-auto translate-y-[5%] lg:translate-y-[10%]">
+        <div className="flex flex-col justify-between max-w-[37.5rem] mx-auto translate-y-[5%] lg:translate-y-[12%]">
             <Header />
             <Form />
             <TodoList />
             <TodoControls />
         </div>
     );
-
 }
 
 export default App;
